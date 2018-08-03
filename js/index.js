079b579e5bbac6ec7dc0eb6a0065cd51c3e388c4
Tsar Andriy <tsarandrij@gmail.com>
2018-08-03 17:04:23 +0000

    Initial

---
+++ js/index.js
@@ -0,0 +1,31 @@
+// (function() {
+//     let counter = 0;
+// function hello() {
+//     counter++;
+//     return counter;
+// }
+
+// let hi = hello();
+// console.log(hi);
+
+// let hi2 = hello();
+// console.log(hi2);
+// })();
+
+
+// function combat(health, damage) {
+//     const result = health - damage;
+//     return result > 0 ? result : 0;
+// }
+
+setTimeout(printMessage, 5000);
+
+function printMessage() {
+    console.log('Hello World');
+}
+
+
+
+
+
+
